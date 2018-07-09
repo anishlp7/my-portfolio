298300875bcc20fce233ac4c4d27c43efb5a1388
anishlp7 <anishlp7@gmail.com>
2018-07-09 15:16:14 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ParticlesOpt from './Background';
+import Particles from 'react-particles-js';
+import Logo from './Components/Logo/Logo';
+import Text from './Components/Text/Text';
+import './Components/Logo/Logo.css';
+import 'tachyons';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Particles className='particles'
+                    params={ParticlesOpt}
+                    />
+                <Text />
+            <Logo />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Components/Text/Text.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Text = () => {
+  return (
+    <div className='a4 mt0'>
+        <p className='f1'>My Portfolio Page</p>
+    </div>
+  );
+}
+
+export default Text;
