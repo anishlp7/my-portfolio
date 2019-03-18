fd9b5ddbba6cc69f83a21114c8768566451a251a
Anish <39819327+anishlp7@users.noreply.github.com>
2019-03-18 08:21:20 +0530

    Updates

---
+++ precache-manifest.593a0225189b48df7db70efab250f310.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "82905d8d1c06969df11c8c378e9bdd4c",
+    "url": "/my-portfolio/static/media/fa-solid-900.82905d8d.svg"
+  },
+  {
+    "revision": "e21222451e106f11c73a",
+    "url": "/my-portfolio/static/css/main.b008d853.chunk.css"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/my-portfolio/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "f377c662080cccf58c1d",
+    "url": "/my-portfolio/static/js/2.90ecd0c1.chunk.js"
+  },
+  {
+    "revision": "c88618ae8fbd8bfc6897f27479383a97",
+    "url": "/my-portfolio/static/media/anish.c88618ae.jpg"
+  },
+  {
+    "revision": "b555dedb35a79aada17f7122044ac58a",
+    "url": "/my-portfolio/static/media/robofriends.b555dedb.png"
+  },
+  {
+    "revision": "7c41ce9e02d23a424c47e7d78ec5eba9",
+    "url": "/my-portfolio/static/media/smartbrain.7c41ce9e.png"
+  },
+  {
+    "revision": "eb41dbf9b56b65be20db0863b9f1714c",
+    "url": "/my-portfolio/static/media/pas.eb41dbf9.png"
+  },
+  {
+    "revision": "bc6177fec2b65cd1b1051b671768160b",
+    "url": "/my-portfolio/static/media/bgcolor.bc6177fe.png"
+  },
+  {
+    "revision": "4b115e1153a9ea339d6a0bb284cc8ed3",
+    "url": "/my-portfolio/static/media/fa-brands-400.4b115e11.woff2"
+  },
+  {
+    "revision": "d9d17590c975aad1be0ddab673f9c769",
+    "url": "/my-portfolio/static/media/fa-brands-400.d9d17590.eot"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/my-portfolio/static/media/fa-brands-400.c39278f7.ttf"
+  },
+  {
+    "revision": "b90365bccdabd68c6c03902b4b141f09",
+    "url": "/my-portfolio/static/media/fa-brands-400.b90365bc.woff"
+  },
+  {
+    "revision": "414ff5daad323a1c47c5177d4bd29674",
+    "url": "/my-portfolio/static/media/fa-regular-400.414ff5da.eot"
+  },
+  {
+    "revision": "5dd3976cb5d61e2e561f2a46b916f377",
+    "url": "/my-portfolio/static/media/fa-regular-400.5dd3976c.woff"
+  },
+  {
+    "revision": "65779ebcc35604a25c2ba77309c5b8af",
+    "url": "/my-portfolio/static/media/fa-regular-400.65779ebc.woff2"
+  },
+  {
+    "revision": "f6c6f6c8cb7784254ad00056f6fbd74e",
+    "url": "/my-portfolio/static/media/fa-regular-400.f6c6f6c8.ttf"
+  },
+  {
+    "revision": "e7e957c87c454bccaa3bf9fdaa6874f8",
+    "url": "/my-portfolio/static/media/fa-regular-400.e7e957c8.svg"
+  },
+  {
+    "revision": "80533988ff5fecd5be26557d08ce8237",
+    "url": "/my-portfolio/static/media/fa-brands-400.80533988.svg"
+  },
+  {
+    "revision": "b5596f4d339f99e3d69bc41be78db962",
+    "url": "/my-portfolio/static/media/fa-solid-900.b5596f4d.eot"
+  },
+  {
+    "revision": "462806316fea535a6a57651bc2b000b0",
+    "url": "/my-portfolio/static/media/fa-solid-900.46280631.woff2"
+  },
+  {
+    "revision": "61969d433bf265b9717a6c357a1e04e4",
+    "url": "/my-portfolio/static/media/fa-solid-900.61969d43.woff"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/my-portfolio/static/media/fa-solid-900.b70cea03.ttf"
+  },
+  {
+    "revision": "e21222451e106f11c73a",
+    "url": "/my-portfolio/static/js/main.a26ef1c7.chunk.js"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/my-portfolio/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/my-portfolio/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/my-portfolio/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/my-portfolio/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/my-portfolio/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/my-portfolio/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/my-portfolio/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/my-portfolio/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/my-portfolio/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/my-portfolio/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/my-portfolio/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/my-portfolio/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/my-portfolio/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/my-portfolio/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/my-portfolio/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/my-portfolio/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/my-portfolio/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/my-portfolio/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/my-portfolio/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/my-portfolio/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/my-portfolio/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "f377c662080cccf58c1d",
+    "url": "/my-portfolio/static/css/2.5331a3bc.chunk.css"
+  },
+  {
+    "revision": "b850dee9caaf4b6801f442eeea587de7",
+    "url": "/my-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/build/precache-manifest.4ba5905fd9320f1b3bed6ad6c959578a.js"
+  "/my-portfolio/precache-manifest.593a0225189b48df7db70efab250f310.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-portfolio/build/index.html", {
+workbox.routing.registerNavigationRoute("/my-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
